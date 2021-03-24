376918cec94b3b9a081efb3ab671ee1212f9ac1f
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-03-24 12:21:20 +0300

    html content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "./components/Carousel";
 import "./Styles.css";
+import image from './images/mountains.jpg'
 
 
 const styles = {
@@ -16,7 +17,19 @@ const styles = {
 const App = () => {
     return (
         <div className="App" style={styles.app}>
-            <Carousel />
+            <Carousel
+                slideHeigth={600}
+                slideWidth={1000}
+            >
+                <img height="100%" width="100%" src={image} alt="someImage" />
+                <h2>Text content</h2>
+                <div>
+                    <h1>awdwa</h1>
+                    <p>awdwa</p>
+                    <div>awdwa</div>
+                </div>
+                {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/o_Ay_iDRAbc" title="YouTube video player" frameBorder="0" allow="accelerometer; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
+            </Carousel>
         </div>
     );
 };
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,44 +1,83 @@
 import React, { useEffect, useRef, useState } from 'react'
-import CarouselItem from './CarouselItem'
 
 const styles = {
+    wrapper: {
+        position: 'relative',
+    },
     viewPort: {
         display: 'flex',
-        width: `400px`,
-        height: '200px',
         background: 'red',
         overflow: 'hidden',
     },
     itemContainer: {
         display: 'flex',
-    }
+    },
+    buttons: {
+        left: {
+            position: 'absolute',
+            top: '50%',
+            left: '0',
+            zIndex: '2',
+        },
+        right: {
+            position: 'absolute',
+            top: '50%',
+            right: '0',
+        },
+    },
 }
 
-const Carousel = () => {
+const Carousel = ({ children, slideWidth, slideHeigth }) => {
     const [currentItem, setCurrentItem] = useState(2)
     const itemContainer = useRef(null)
+    const viewPort = useRef(null)
+    const [newChildren, setNewChildren] = useState(children)
+
+    const itemStyles = {
+        height: `${slideHeigth}px`,
+        width: `${slideWidth}px`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+    }
+
+
 
     useEffect(() => {
+        window.addEventListener('resize', () => {
+            console.log('resize')
+            if (window.innerWidth < slideWidth) {
+
+            }
+        })
+
+        // return window.removeEventListener('resize',)
+    }, [])
+
+
+    useEffect(() => {
+        viewPort.current.style.maxWidth = `${slideWidth}px`
+        viewPort.current.style.maxHeight = `${slideHeigth}px`
+
         let firstItem = itemContainer.current.children[0].cloneNode(true)
         let lastItem = itemContainer.current.children[itemContainer.current.children.length - 1].cloneNode(true)
 
         itemContainer.current.insertBefore(lastItem, itemContainer.current.children[0])
         itemContainer.current.append(firstItem)
 
-        itemContainer.current.style.transform = `translate(-${400}px)`
+        itemContainer.current.style.transform = `translateX(-${slideWidth}px)`
         itemContainer.current.style.transitionDuration = `0.0s`
     }, [])
 
     const nextItemHandler = () => {
         if (currentItem < itemContainer.current.children.length) {
             setCurrentItem(prev => prev + 1)
-            itemContainer.current.style.transform = `translate(-${400 * currentItem}px)`
+            itemContainer.current.style.transform = `translateX(-${slideWidth * currentItem}px)`
             itemContainer.current.style.transitionDuration = `0.7s`
 
             if (currentItem === itemContainer.current.children.length - 1) {
                 setTimeout(() => {
                     itemContainer.current.style.transitionDuration = `0.0s`
-                    itemContainer.current.style.transform = `translate(-${400}px)`
+                    itemContainer.current.style.transform = `translateX(-${slideWidth}px)`
                     setCurrentItem(2)
                 }, 700)
 
@@ -52,13 +91,13 @@ const Carousel = () => {
     const previousItemHandler = () => {
         setCurrentItem(prev => prev - 1)
         if (currentItem > 0) {
-            itemContainer.current.style.transform = `translate(-${400 * currentItem - 2 * 400}px)`
+            itemContainer.current.style.transform = `translateX(-${slideWidth * currentItem - 2 * slideWidth}px)`
             itemContainer.current.style.transitionDuration = `0.7s`
 
             if (currentItem === 2) {
                 setTimeout(() => {
                     itemContainer.current.style.transitionDuration = `0.0s`
-                    itemContainer.current.style.transform = `translate(-${400 * (itemContainer.current.children.length - 2)}px)`
+                    itemContainer.current.style.transform = `translateX(-${slideWidth * (itemContainer.current.children.length - 2)}px)`
                     setCurrentItem(itemContainer.current.children.length - 1)
                 }, 700)
 
@@ -71,17 +110,14 @@ const Carousel = () => {
 
 
     return (
-        <div>
-            <button onClick={previousItemHandler}>Previous</button>
-            <button onClick={nextItemHandler}>Next</button>
-            <div style={styles.viewPort}>
-                <div ref={itemContainer} style={styles.itemContainer}>
-                    <CarouselItem number={0} />
-                    <CarouselItem number={1} />
-                    <CarouselItem number={2} />
-                    <CarouselItem number={3} />
+        <div style={styles.wrapper}>
+            <button style={styles.buttons.left} onClick={previousItemHandler}>Previous</button>
+            <div ref={viewPort} style={styles.viewPort}>
+                <div onTouchStart={() => console.log('move')} ref={itemContainer} style={styles.itemContainer}>
+                    {newChildren.map((item, i) => <div key={i} style={itemStyles}>{item}</div>)}
                 </div>
             </div>
+            <button style={styles.buttons.right} onClick={nextItemHandler}>Next</button>
         </div>
     )
 }
