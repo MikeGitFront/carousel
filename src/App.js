d3dd3768ac3c031d0c072aa909c42a65a94da88f
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-03-25 09:08:33 +0300

    beauty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Carousel from "./components/Carousel";
 import "./Styles.css";
-import image from './images/mountains.jpg'
 import styled from 'styled-components'
+import picture from './images/mountains.jpg'
+import picture1 from './images/mountains1.jpg'
+import picture2 from './images/mountains2.jpg'
+import picture3 from './images/mountains3.jpg'
 
 const AppWrapper = styled.div`
 display:flex;
@@ -10,17 +13,20 @@ justify-content:center;
 align-items:center;
 height:100vh;
 width:100vw;
-
-@media(max-width:1000px) {
-    background-color:black;
-}
 `
 
 const App = () => {
     return (
         <AppWrapper>
-            <Carousel>
-
+            <Carousel infinite>
+                <div src={picture} ></div>
+                <div src={picture1} ></div>
+                <div src={picture2} ></div>
+                <div src={picture3} ></div>
+                <div align="flex-start"
+                    padding="4px 10px">
+                    <h1>Hello</h1>
+                </div>
             </Carousel>
         </AppWrapper>
     );
