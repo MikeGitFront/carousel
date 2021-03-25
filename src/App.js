01180e35de03cf9904f7c9540870b02bebd34d04
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-03-25 21:33:39 +0300

    desktop swipe

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from "react";
 import Carousel from "./components/Carousel";
 import "./Styles.css";
-import styled from 'styled-components'
 import picture from './images/mountains.jpg'
 import picture1 from './images/mountains1.jpg'
 import picture2 from './images/mountains2.jpg'
 import picture3 from './images/mountains3.jpg'
 
-const AppWrapper = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-height:100vh;
-width:100vw;
-`
-
 const App = () => {
     return (
-        <AppWrapper>
-            <Carousel infinite>
-                <div src={picture} ></div>
-                <div src={picture1} ></div>
-                <div src={picture2} ></div>
-                <div src={picture3} ></div>
-                {/* <div align="flex-start"
-                    padding="4px 10px">
-                    <h1>Hello</h1>
-                </div> */}
-            </Carousel>
-        </AppWrapper>
+        <Carousel infinite>
+            <div src={picture} ></div>
+            <div src={picture1} ></div>
+            <div src={picture2} ></div>
+            <div src={picture3} ></div>
+            <div
+                align="flex-start"
+                padding="4px 10px"
+                bgcolor="green"
+            >
+                <h1>Some content</h1>
+            </div>
+            <div
+                bgcolor="red"
+            ><iframe width="560" height="315" src="https://www.youtube.com/embed/o_Ay_iDRAbc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
+        </Carousel>
     );
 };
 
